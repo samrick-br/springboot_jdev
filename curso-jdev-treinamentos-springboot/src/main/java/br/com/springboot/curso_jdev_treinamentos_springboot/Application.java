7fd59f6c30d6c9590b75ee605162418205b4372d
Samrick <ricrdocardoso@gmail.com>
2023-09-01 20:18:55 -0300

    Configuração inicial e conexão banco de dados Postgres 15

---
+++ curso-jdev-treinamentos-springboot/src/main/java/br/com/springboot/curso_jdev_treinamentos_springboot/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.curso_jdev_treinamentos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
