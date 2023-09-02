104b51520012b170abcfc7642e274ca8c2d7e431
Samrick <ricrdocardoso@gmail.com>
2023-09-01 23:10:21 -0300

    Método de buscar e atualizar usuario
    Método de buscar usuário por ID;
    Método de atualização de usuário com validação para não atualizar sem ID;

--- curso-jdev-treinamentos-springboot/src/main/java/br/com/springboot/curso_jdev_treinamentos_springboot/controllers/GreetingsController.java
+++ curso-jdev-treinamentos-springboot/src/main/java/br/com/springboot/curso_jdev_treinamentos_springboot/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -73,13 +74,35 @@ public class GreetingsController {
      	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
     
+    @PutMapping(value = "atualizar") /* Mapeia a url*/
+    @ResponseBody /* Descrição da resposta*/
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario){ /* Recebe os dados para salvar*/
+    	
+    	if (usuario.getId() == null) {
+    		return new ResponseEntity<String>("Id não informado, impossível atualizar.", HttpStatus.CREATED);
+		}
+    	
+     	Usuario user = usuarioRepository.saveAndFlush(usuario);
+     	
+     	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+    
     
     @DeleteMapping(value = "delete") /* Mapeia a url*/
     @ResponseBody /* Descrição da resposta*/
     public ResponseEntity<String> delete(@RequestParam Long iduser){ /* Recebe os dados para salvar*/
     	
-     	usuarioRepository.deleteById(iduser);;
+     	usuarioRepository.deleteById(iduser);
      	
      	return new ResponseEntity<String>("usuário deletado com sucesso!", HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "buscaruserid") /* Mapeia a url*/
+    @ResponseBody /* Descrição da resposta*/
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser){ /* Recebe os dados para salvar*/
+    	
+     	Usuario usuario = usuarioRepository.findById(iduser).get();
+     	
+     	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
     }
 }
